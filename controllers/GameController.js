aa048f722fcd750c4fa7ece22c72eb7e3907c423
Carlos Jaime <jaime_andrek@hotmail.com>
2022-05-20 16:15:52 -0300

    method put e patch

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -55,14 +55,87 @@ const newGame = (req, res)=>{
     if(year === undefined  || isNaN(year)) return res.sendStatus(400)
     if(price === undefined  || isNaN(price)) return  res.sendStatus(400)
 
-   
+
     DB.games.push({id, title, year, price}) 
+
     res.sendStatus(200)
 
+}
+
+const destroy = (req,res) => {
+    const id = req.body.id
+
+    if(id === undefined || isNaN(id)) return res.sendStatus(400) 
+
+    const index = DB.games.findIndex(g => g.id == id)
+
+    if(index === -1) return res.sendStatus(400) 
+
+    DB.games.splice(index, 1)
+    res.sendStatus(200)
+
+}
+
+const update = (req,res)=>{
+    const id = req.params.id;
+
+    if(isNaN(id)) return res.sendStatus(400);
+
+    const game = DB.games.find(g=> g.id == id)
+
+    if(!game) return res.sendStatus(404);
+
+    const {title, year, price} = req.body
+
+    if(title === undefined && year === undefined  && price === undefined ) return res.sendStatus(400)
+    if(title === undefined) return res.sendStatus(400)
+    if(year === undefined  || isNaN(year)) return res.sendStatus(400)
+    if(price === undefined  || isNaN(price)) return  res.sendStatus(400)
+
+    game['title'] = title
+    game['year'] = year
+    game['price'] = price
+
+    res.sendStatus(200)
+
+}
+
+const updateParcial = (req,res) => {
+    const id = req.params.id;
+
+    if(isNaN(id)) return res.sendStatus(400);
+
+    const game = DB.games.find(g=> g.id == id)
+
+    if(!game) return res.sendStatus(404);
+
+    const {title, year, price} = req.body
+
+    if(title != undefined && year != undefined  && price != undefined ) return res.sendStatus(400)
+    if(title === undefined && year === undefined  && price === undefined ) return res.sendStatus(400)
+
+    if(title != undefined){
+        game['title'] = title
+    }
+
+    if(year != undefined){
+        if(isNaN(year)) return res.sendStatus(400)
+        game['year'] = year
+    }
+
+    if(price != undefined){
+        if(isNaN(price)) return res.sendStatus(400)
+        game['price'] = price
+    }
+
+    res.sendStatus(200)
 }
 
 module.exports = {
     games,
     game,
-    newGame
+    newGame,
+    destroy,
+    update,
+    updateParcial
 }
--- routes/game/index.js
+++ routes/game/index.js
@@ -5,5 +5,8 @@ const gameController = require("../../controllers/GameController")
 router.get("/games",gameController.games)
 router.get("/game/:id",gameController.game)
 router.post("/game/",gameController.newGame)
+router.delete("/game/",gameController.destroy)
+router.put("/game/:id",gameController.update)
+router.patch("/game/:id",gameController.updateParcial)
 
 module.exports = router
